b744dd8647ef2d17cc0a8c1b0b10395479b6eabb
Li Tao <39371031+zer0stack@users.noreply.github.com>
2020-05-27 11:28:15 +0300

    Add files via upload

--- kbstate.c
+++ kbstate.c
@@ -8,23 +8,13 @@
 #include <X11/XKBlib.h>
 #include <X11/extensions/XKBrules.h>
 
-#define MAXLEN 32
+#define MAXLEN 8
 #define INTERVAL 1
 
 /* Compile with:
  * gcc -I/usr/include kbstate.c -lX11 -lxkbfile -o kbstate */
 
-int str_to_upper(char *str) {
-  if (str != NULL) {
-    for (char *p = str; *p; p++)
-      *p = toupper((unsigned char) *p);
-    return EXIT_SUCCESS;
-  } 
-  else
-    return EXIT_FAILURE;
-}
-
-const char *get_layout(Display *dpy, bool up) {
+int put_infos(Display *dpy, char *pref, bool isUpper) {
   XkbStateRec state;
   XkbGetState(dpy, XkbUseCoreKbd, &state);
 
@@ -36,20 +26,21 @@ const char *get_layout(Display *dpy, bool up) {
   for (int i = 0; i < state.group; i++) {
     tok = strtok(NULL, ",");
     if (tok == NULL)
-      return NULL;
+      return EXIT_FAILURE;
   }
-
-  if (up)
-    str_to_upper(tok);
-
-  return tok;
-}
-
-int put_infos(char *pref, const char *layout) {
-  char buf[MAXLEN];
   
-  if (snprintf(buf, sizeof(buf), "%s%s", pref, layout) > 0 &&
-      puts(buf) > 0 &&
+  if (isUpper)
+    for (char *p = tok; *p; p++)
+      *p = toupper((unsigned char) *p);
+  
+  char buf[MAXLEN];
+  int concat_code;
+  
+  concat_code = snprintf(buf, sizeof(buf), "%s%s", pref, tok);
+ 
+  if (concat_code > -1 &&
+      concat_code < sizeof(buf) &&
+      puts(buf) != EOF &&
       fflush(stdout) != EOF)
     return EXIT_SUCCESS;
   else
@@ -65,7 +56,7 @@ int main(int argc, char *argv[]) {
   }
 
   char *pref = "";
-  bool up = false;
+  bool isUpper = false;
   int interval = INTERVAL;
   bool snoop = false;
   
@@ -80,7 +71,7 @@ int main(int argc, char *argv[]) {
         snoop = true;
         break;
       case 'u':
-        up = true;
+        isUpper = true;
         break;
       case 'p':
         pref = optarg;
@@ -88,18 +79,13 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (strlen(pref) + 2 + 1 > MAXLEN) {
-    fprintf(stderr, "Result string is too long.\n");
-    exit(EXIT_FAILURE);
-  }
-
   int exit_code;
 
   if (snoop)
-    while ((exit_code = put_infos(pref, get_layout(dpy, up))) != EXIT_FAILURE)
+    while ((exit_code = put_infos(dpy, pref, isUpper)) != EXIT_FAILURE)
       sleep(interval);
     else
-      exit_code = put_infos(pref, get_layout(dpy, up));
+      exit_code = put_infos(dpy, pref, isUpper);
 
   XCloseDisplay(dpy);
   return exit_code;
